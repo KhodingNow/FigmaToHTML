7a3ed454c9e8cd7b3f63ab1b19b529ed2a52db81
my name <me@example.com>
2024-09-18 09:33:03 +0200

    wenzani

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleMenu(displayState){
+    const nav = document.getElementById('header_navigation');
+    const closeIco = document.getElementById('close-ico');
+
+    nav.style.display = displayState;
+    closeIco.style.display = displayState;
+
+}   
